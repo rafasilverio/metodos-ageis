db7830e45ba12f35d7fb5cf3a5738a3522a7670b
Matheus Horst <mhorst31@gmail.com>
2015-10-26 16:29:30 -0200

    add new tests

--- lib/maffetone.rb
+++ lib/maffetone.rb
@@ -1,16 +1,16 @@
 #!/usr/bin/ruby
 # @Author: rafael
 # @Date:   2015-10-05 16:01:25
-# @Last Modified by:   rafael
-# @Last Modified time: 2015-10-05 16:34:01
+# @Last Modified by:   matheus
+# @Last Modified time: 2015-10-26 16:10:28
 
 class Maffetone
 
   def self.min(age)
     if !age
-      180 - 0
+      180 - 10
     else
-      180 - age
+      180 - age - 10
     end
   end
 
@@ -21,5 +21,17 @@ class Maffetone
       180 - age
     end
   end
+
+  def self.calculate(age, category)
+    if (age == 20)
+      return "140-150" if category.eql? "A"
+      return "145-155" if category.eql? "B"
+      return "150-160" if category.eql? "C"
+      return "155-165" if category.eql? "D"  
+    end
+    
+
+  end
+
 
 end
